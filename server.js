90a94d3314383d9599c839ce12c64559545eae32
DreamCat04 <112375774+DreamCat04@users.noreply.github.com>
2023-09-08 17:11:14 +0200

    Implemented sending a message

--- server.js
+++ server.js
@@ -16,18 +16,37 @@ app.use(session({
 }));
 
 const port = 5000;
-const usersData = JSON.parse(fs.readFileSync('./users.json', 'utf8'));
-const users = usersData.users;
+
+let messagesData = [];
+const filePath = './messages.json';
+
+try {
+  const fileData = fs.readFileSync(filePath, 'utf8');
+  messagesData = JSON.parse(fileData);
+} catch (err) {
+  // Handle the error, e.g., if the file doesn't exist or is not valid JSON
+  console.error('Error reading file:', err);
+}
+
+// Function to generate an incremented ID
+const generateNextId = () => {
+    if (messagesData.messages.length === 0) {
+      return 1;
+    } else {
+      const lastItem = messagesData.messages[messagesData.messages.length - 1];
+      return lastItem ? lastItem.id + 1 : 1; // Check if lastItem is defined
+    }
+  };
 
 const messages = JSON.parse(fs.readFileSync('./messages.json', 'utf8'));
 app.post("/login", (request, response) => {
     const username = request.body.username;
     const password = request.body.password;
-    console.log('Received username:', username);
-    console.log('Received password:', password);
-    
-  // Read the users from the users.json file
 
+  // Read the users from the users.json file
+  const usersData = JSON.parse(fs.readFileSync('./users.json', 'utf8'));
+  const users = usersData.users;
+  
   // Check if the provided credentials match any user in the users.json file
   const foundUser = users.find((user) => {
     return user.username === username && user.password === password;
@@ -44,15 +63,33 @@ app.post("/login", (request, response) => {
   }
 });
 app.get("/messages", (request, response) => {
-const loggedInUser = request.session.username;
-response.json(messages.messages);
+    //temporarily done with a messages file, will later be replaced with a database call
+    const messages = JSON.parse(fs.readFileSync('./messages.json', 'utf8'));
+    const loggedInUser = request.session.username;
+    response.json(messages.messages);
 });
 //send a message
 app.post("/messages", (request, response) => {
+    //temporarily done with a messages file, will later be replaced with a database call
+    const newMessage = request.body;
+    //console.log(newMessage);
+    // Generate an incremented ID
+    newMessage.id = generateNextId();
+    newMessage.sentBy = request.session.username;
+    newMessage.sentAt = new Date().toISOString;
+    // Add the new data to the array
+    messagesData.messages.push(newMessage);
+  
+    // Write the updated data back to the file
+    fs.writeFileSync(filePath, JSON.stringify(messagesData, null, 2), 'utf8');
+  
+    response.status(201).json(newMessage); // Respond with the added data
 
 });
 //update a certain message by its ID
 app.put("/messages/:id", (request, response) => {
+const updateContent = request.body.message;
+const messageIdToUpdate = request.params.id;
 
 });
 
