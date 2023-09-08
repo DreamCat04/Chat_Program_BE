1478089dd8d6556a2f5c3a130d3ead314f27b818
DreamCat04 <112375774+DreamCat04@users.noreply.github.com>
2023-09-08 13:53:18 +0200

    Create sample data & finish server file

--- server.js
+++ server.js
@@ -1,1 +1,28 @@
-const express = use(express);
+const express = require('express');
+const app = express();
+const session = require('express-session');
+
+app.use(express.urlencoded({'extended' : true}));
+
+const port = 3000;
+
+app.post("/login", (request, response) => {
+	const username = request.body.username;
+	const password = request.body.password;
+	request.session.username = request.body.username;
+	request.session.password = request.body.password;
+	if(/*insert check for correct login credentials*/true) {
+		authenticated = true;
+		currentlyLoggedInUser = username;
+		response.status(200).send('Login successful!');
+	} else {
+		response.status(401).send('Invalid login credentials!');
+	}
+});
+app.get("/messages", (request, response) => {
+
+});
+
+app.listen(port, () => {
+	console.log(`The application successfully started on port ${port}`);
+});
