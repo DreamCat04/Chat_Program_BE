3dd2cacf80ae5199da63cd0f347f5d75989b11b8
DreamCat04 <112375774+DreamCat04@users.noreply.github.com>
2023-11-17 13:08:57 +0100

    Implement correct structure for init.sql

--- init.sql
+++ init.sql
@@ -1,11 +1,21 @@
+-- Create the chat_db database if it doesn't exist
+CREATE DATABASE IF NOT EXISTS chat_db;
+
+USE chat_db
+
+-- Create the User table
 CREATE TABLE IF NOT EXISTS User (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    Name VARCHAR(255) NOT NULL,
-    Email VARCHAR(255) NOT NULL
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  username VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL
 );
+
+-- Create the Message table with a foreign key
 CREATE TABLE IF NOT EXISTS Message (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    content VARCHAR(255) NOT NULL,
-    sent_at DATE NOT NULL,
-    FOREIGN KEY (fk_message_id) REFERENCES User(id)
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  content TEXT NOT NULL,
+  sentAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  sent_by INT,
+  FOREIGN KEY (sent_by) REFERENCES User(id)
 );
